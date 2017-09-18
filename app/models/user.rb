2f80a9e038e1ad81462f94e1a1c5bda16a91b75c
Mark Delos Reyes <makizworks@gmail.com>
2017-09-18 14:35:00 -0400

    add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 40 }
-  validates :email, presence: true, length: { maximum: 200 }
+  validates :email, presence: true, length: { maximum: 200 }, 
+                                    uniqueness: { case_sensitive: false }
 end
