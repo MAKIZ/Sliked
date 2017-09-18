7bc7b97b2e7766d9f26f8822de8aa95adb963e85
Mark Delos Reyes <makizworks@gmail.com>
2017-09-17 23:17:08 -0400

    test index route, prep for heroku deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "If you see this in the browser you are awesome!"
+  end
 end
