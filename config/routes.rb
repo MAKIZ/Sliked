8273917cf83968a55e1a5470be4466f3537d3a0a
Mark Delos Reyes <makizworks@gmail.com>
2017-09-18 14:12:12 -0400

    add index to user email

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :posts
   root to: 'posts#index'
-  
   resources :users, only: [:new, :create, :index, :show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170918174824) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
