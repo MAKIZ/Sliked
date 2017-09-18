324fb3c78a8b5710849da22b062b1e4ef38f0224
Mark Delos Reyes <makizworks@gmail.com>
2017-09-18 16:05:23 -0400

    add session controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,17 @@ class User < ApplicationRecord
                                     uniqueness: { case_sensitive: false }
 
   has_secure_password
+  def is_password?(password_attempt)
+    BCrypt::Password.new(password_digest).is_password?(password_attempt)
+  end
+
+  def password=(raw_password)
+    self.password_digest = BCrypt::Password.create(raw_password)
+  end
+
+  def self.find_from_credentials(email, password)
+    user = find_by(email: email) # same as User.find_by( ... )
+    return nil unless user
+    user if user.is_password?(password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts
   root to: 'posts#index'
+  resources :posts
   resources :users, only: [:new, :create, :index, :show]
+  resource :session, only: [:new, :create, :destroy]
 end
