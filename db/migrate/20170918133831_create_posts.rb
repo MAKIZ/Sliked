ab7445fa343181378c7a0d74b2705d2505e0a8d5
Mark Delos Reyes <makizworks@gmail.com>
2017-09-18 09:50:22 -0400

    add post model

---
+++ db/migrate/20170918133831_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.text :description
+      t.string :photo
+      t.belongs_to :user, index: true
+
+      t.timestamps
+    end
+  end
+end
