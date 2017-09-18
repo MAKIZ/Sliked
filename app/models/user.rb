7786535b872b5033b76eeb0e197c131f221b7aa7
Mark Delos Reyes <makizworks@gmail.com>
2017-09-18 15:50:17 -0400

    user authentication, new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
 
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 40 }
-  validates :email, presence: true, length: { maximum: 200 }, 
+  validates :email, presence: true, length: { maximum: 254 },
                                     uniqueness: { case_sensitive: false }
+
+  has_secure_password
 end
